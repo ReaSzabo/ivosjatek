54a1303bd5ee38ad729369b08cb91ca47e4cc329
ReaSzabo <szaboandreaannaa@gmail.com>
2024-03-12 23:11:18 +0100

    Set up the routing (#2)

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Menu from "./Menu.jsx";
+import OtherGames from "./OtherGames.jsx";
+import Game from "./Game.jsx";
+import Contact from "./Contact.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Menu></Menu>,
+  },
+  {
+    path: "/OtherGames",
+    element: <OtherGames></OtherGames>,
+  },
+  {
+    path: "/Game",
+    element: <Game></Game>,
+  },
+  {
+    path: "/Contact",
+    element: <Contact></Contact>,
+  },
+]);
+
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
