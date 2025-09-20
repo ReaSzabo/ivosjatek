48c69c0b267bbb07271af6739e365788abc878a0
Andrea Szabo <szaboandreaannaa@gmail.com>
2025-09-20 19:29:44 +0200

    Next.js migration (#28)

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -2,7 +2,6 @@ import { Component } from "react";
 import styles from "./Menu.module.scss";
 import Card from "./Card";
 import Link from 'next/link';
-import CookieConsent from "react-cookie-consent";
 
 class Menu extends Component {
   render() {
@@ -10,43 +9,27 @@ class Menu extends Component {
       <div className={styles["menu-container"]}>
         <div className={styles["menu-container__elements"]}>
 
-          <Link href="/mas-ivos-jatekok" legacyBehavior>
-            <a>
-              <Card
-                image="/assets/images/menu/cat.webp"
-                imageRemoved="/assets/images/menu/catRemoved.webp"
-                caption="Más Ivós Játékok"
-              />
-            </a>
+          <Link href="/mas-ivos-jatekok" >
+            <Card
+              image="/assets/images/menu/cat.webp"
+              imageRemoved="/assets/images/menu/catRemoved.webp"
+              caption="Más Ivós Játékok"
+            />
           </Link>
-          <Link href="/jatek" legacyBehavior>
-            <a>
-              <Card
-                image="/assets/images/menu/lion.webp"
-                imageRemoved="/assets/images/menu/lionRemoved.webp"
-                caption="Ivós Játék"
-              />
-            </a>
+          <Link href="/jatek" >
+            <Card
+              image="/assets/images/menu/lion.webp"
+              imageRemoved="/assets/images/menu/lionRemoved.webp"
+              caption="Ivós Játék"
+            />
           </Link>
-          <Link href="/kapcsolat" legacyBehavior>
-            <a>
-              <Card
-                image="/assets/images/menu/dog.webp"
-                imageRemoved="/assets/images/menu/dogRemoved.webp"
-                caption="Kapcsolat"
-              />
-            </a>
+          <Link href="/kapcsolat" >
+            <Card
+              image="/assets/images/menu/dog.webp"
+              imageRemoved="/assets/images/menu/dogRemoved.webp"
+              caption="Kapcsolat"
+            />
           </Link>
-          <CookieConsent
-            location="bottom"
-            buttonText="Hát persze!!"
-            cookieName="myAwesomeCookieName2"
-            style={{ background: "#ecb603" }}
-            buttonStyle={{ background: "#fff1ff", color: "#02a4d1", borderRadius: "4px", fontSize: "13px", fontFamily: "Montserrat, sans-serif", fontVariant: "all-small-caps" }}
-            expires={150}
-          >
-            Sütiket (cookie-kat) használunk a választott nyelv mentéséhez. Hozzájárulsz?
-          </CookieConsent>
         </div>
       </div >
     );
