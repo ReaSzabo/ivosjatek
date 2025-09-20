fec39b2e4a6bf86e8c5b3e6be6f0ee644dfce0a3
Andrea Szabo <szaboandreaannaa@gmail.com>
2025-09-20 20:39:12 +0200

    Next.js migration (#28)

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     reactStrictMode: true,
     images: {
         formats: ['image/webp'],
+        unoptimized: true,
     },
     devIndicators: {
         autoPrerender: false,
     },
     // Hide Next.js dev overlay
     reactDevOverlay: false,
+    output: 'export',
 };
 
 module.exports = nextConfig;
