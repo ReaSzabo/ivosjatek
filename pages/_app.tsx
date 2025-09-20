7e2ad9ab5c1c69c8e9b39133e8e397496989f42a
Andrea Szabo <szaboandreaannaa@gmail.com>
2025-09-20 20:22:53 +0200

    Next.js migration (#28)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,14 @@
 
-import '../src/styles/globals.scss';
+import "../src/styles/globals.scss";
+
+import CustomCookieConsent from '../src/components/CustomCookieConsent';
 import type { AppProps } from 'next/app';
-import CookieConsent from 'react-cookie-consent';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
-            <CookieConsent
-                location="bottom"
-                buttonText="Hát persze!!"
-                cookieName="myAwesomeCookieName2"
-                style={{ background: "#ecb603" }}
-                buttonStyle={{ background: "#fff1ff", color: "#02a4d1", borderRadius: "4px", fontSize: "13px", fontFamily: "Montserrat, sans-serif", fontVariant: "all-small-caps" }}
-                expires={150}
-            >
-                Sütiket (cookie-kat) használunk a választott nyelv mentéséhez. Hozzájárulsz?
-            </CookieConsent>
+            <CustomCookieConsent />
         </>
     );
 }
